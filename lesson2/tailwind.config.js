2871fc0ce5e4a263b20eecfe273672106dfe607b
Hussam812 <hossamhosnihussien@gmail.com>
2023-11-01 09:19:28 +0200

    finsh all pages start on menu

--- lesson2/tailwind.config.js
+++ lesson2/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        widescreen: { row: "(min-aspect-ratio: 3/2" },
+        tallscreen: { row: "(min-aspects-ratio:1/2" },
+      },
+    },
   },
   plugins: [],
 };
